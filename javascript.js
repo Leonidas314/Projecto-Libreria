6454ef7852b0a5ab5b9d3081bb42200b4871dd2d
Leon <leondellafiore2016@gmail.com>
2024-07-07 18:02:14 -0300

    add funtionalities to newButton: display form, cancelButton: undisplay form

--- javascript.js
+++ javascript.js
@@ -12,11 +12,14 @@ function NewBook(title,author,pages,read){
 }
 
 const newButton = document.getElementById('new-button');
-
+//Aparece el formulario para cargar el libro nuevo
 newButton.addEventListener('click',function(){
+    const formScreen = document.getElementById('backScreenForm');
+    formScreen.style.display="flex";
+})
 
-    const bookdiv = document.createElement('div');
-    bookdiv.className= 'book';
-    const bookContainer = document.getElementById('book-container')
-    bookContainer.appendChild(bookdiv)
+const cancelButton = document.getElementById('cancelButton');
+cancelButton.addEventListener('click',function(){
+    const formScreen = document.getElementById('backScreenForm');
+    formScreen.style.display="none";
 })
